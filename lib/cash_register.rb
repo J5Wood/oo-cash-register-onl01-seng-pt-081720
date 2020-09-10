e07883362c68865d6b3c58a938ad2e01a5fe6bf9
Jeremy Wood <js.wood206@gmail.com>
2020-09-10 06:24:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class CashRegister
 
   attr_accessor :total, :discount
@@ -9,8 +11,10 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-
   end
 
+  def apply_discounts
+    binding.pry
+  end
 
 end
